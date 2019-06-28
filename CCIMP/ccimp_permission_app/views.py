5cbfcd5376f43a5461c26bc2de48d4fd3376425d
zhangbo666 <78038490@qq.com>
2019-06-28 20:31:58 +0800

    2019-06-28 20:31

--- CCIMP/ccimp_permission_app/views.py
+++ CCIMP/ccimp_permission_app/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
+from django.contrib import messages
 
 from ccimp_user_app.views import auth
 
@@ -42,9 +43,11 @@ def edit_permission(request,uid):
 
 '''###############################################################################'''
 
-# 权限分类
+# 权限分类list
 @auth
 def permission_class(request):
+
+    '''权限分类list'''
 
     get_username = request.session.get('user', '')
 
@@ -60,11 +63,12 @@ def permission_class(request):
 
                 if user.permission_options == 1:
 
-                   return render(request,"permission_class.html",
-                              {"username":get_username,
-                               "type":"list",
-                               "type_option":"permission_sap",
-                               "permissionClasss":permissionClasss})
+                    print (type(user.create_time),user.create_time)
+                    return render(request,"permission_class.html",
+                                {"username":get_username,
+                                 "type":"list",
+                                 "type_option":"permission_sap",
+                                 "permissionClasss":permissionClasss})
 
                 else:
 
@@ -82,40 +86,30 @@ def permission_class(request):
                        "type_option":"permission_sap",
                        "permissionClasss": permissionClasss})
 
-# 添加权限
+# 添加权限分类
 @auth
 def add_permissionClass(request):
 
-    '''创建权限'''
+    '''创建权限分类'''
 
     permission_chinese_names = PermissionClass.objects.all()
 
-    permission_names = []
-
-    username = request.session.get('user', '')
-
-    # 返回前端非超级管理员信息
-    for p1 in permission_chinese_names:
-
-        if p1.permission_options != 1:
-
-            permission_names.append(p1)
+    get_username = request.session.get('user', '')
 
     if request.method == "GET":
 
-        user = User.objects.get(user_name=username)
+        user = User.objects.get(user_name=get_username)
 
         permissionClasss = PermissionClass.objects.all()
 
-        if user.user_name == username:
+        if user.user_name == get_username:
 
             if user.permission_options == 1:
 
                     return render(request,"permission_class_add.html",
-                                  {"username":username,
+                                  {"username":get_username,
                                    "type":"add",
-                                   "type_option":"permission_sap",
-                                   "permission_names":permission_names})
+                                   "type_option":"permission_sap"})
 
             else:
 
@@ -127,31 +121,72 @@ def add_permissionClass(request):
 
     else:
 
-        pc_name = request.POST.get("permission_chine_name","")
+        pc_name = request.POST.get("permission_chine_name", "")
+        pe_name = request.POST.get("permission_english_name", "")
+        po_name = request.POST.get("permission_options", "")
 
-        pe_name = request.POST.get("permission_english_name","")
+        if pc_name == "":
 
-        po_name = request.POST.get("permission_options","")
+            return render(request, "permission_class_add.html", {"username": get_username,
+                                                                 "type": "add",
+                                                                 "type_option": "permission_sap",
+                                                                 "pc_name": "权限名称不能为空！"})
+        elif pe_name == "":
 
-        # po_name = request.POST.get("poName","")
+            return render(request, "permission_class_add.html", {"username": get_username,
+                                                                 "type": "add",
+                                                                 "type_option": "permission_sap",
+                                                                 "pe_name": "权限英文不能为空！"})
+
+        elif po_name == "":
+
+            return render(request, "permission_class_add.html", {"username": get_username,
+                                                                 "type": "add",
+                                                                 "type_option": "permission_sap",
+                                                                 "po_name": "权限分类不能为空！"})
 
         if po_name == "项目管理员" or po_name == "模块管理员" or po_name == "普通管理员":
 
-            return render(request, "permission_class.html",{"username":username,"type":"add"})
+            if po_name == "项目管理员":
+
+                po_name = 2
+
+            elif po_name == "模块管理员":
+
+                po_name = 3
+
+            elif po_name == "普通管理员":
+
+                po_name = 4
+
+            PermissionClass.objects.create(permission_chinese_name=pc_name,
+                                           permission_english_name=pe_name,
+                                           permission_options=po_name)
+
+            return HttpResponseRedirect("/permission/class/")
 
         else:
 
-            return render(request, "permission_class_add.html",{"username":username,"type":"add","po_name":"权限分类错误"})
+            return render(request, "permission_class_add.html", {"username": get_username,
+                                                                 "type": "add",
+                                                                 "type_option": "permission_sap",
+                                                                 "po_name": "权限分类数据错误，请查看原因！"})
 
 
-# 编辑权限
+# 编辑权限分类
 @auth
 def edit_permissionClass(request,pclass_id):
 
-    '''编辑权限'''
+    '''编辑权限分类'''
 
     permissionclass = PermissionClass.objects.get(id=pclass_id)
 
+    get_username = request.session.get('user', '')
+
     if request.method == "GET":
 
-        return render(request,"permission_class_add.html",{"type":"add","permissionclass":permissionclass})
+        return render(request,"permission_class_edit.html",
+                      {"username":get_username,
+                       "type": "edit",
+                       "type_option":"permission_sap",
+                       "permissionClasss": permissionclass})
